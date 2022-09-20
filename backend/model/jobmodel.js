613831d200ab61c32eeed4894f3e7e9f9853cca8
prakash <saiprakashyadav31@gmail.com>
2022-09-20 22:30:39 +0530

    secondcommit

--- backend/model/jobmodel.js
+++ backend/model/jobmodel.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 
 const jobSchema = mongoose.Schema({
+
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
     text:  {
         type: String,
-        require: [true, 'please add a text value']
+        require: [true, 'please add a text value'],
 
-    }
+    },
 } ,{
     timestamps: true,
 }
--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended: false}))
 
 
 app.use('/api/jobs', require('./routes/jobRoutes'))    
-
+app.use('/api/users', require('./routes/userRoutes')) 
 
 
 app.use(errorHandler) 
